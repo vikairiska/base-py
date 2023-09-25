497f4868cbf22d9b0c22562c5886ccfd284c792a
vikairiska <verhozinavika2006@gmail.com>
2023-09-25 22:17:53 +0300

    Add all homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return[number**2 for number in numbers ]
 
 
 # filter types
